4727f573d6f84f4ad48fd1eb47a2717ccbdaaf04
Conley <conleypa@8c859047a39c.ant.amazon.com>
2019-10-20 17:24:28 -0400

    p12 final

--- p12b.py
+++ p12b.py
@@ -42,14 +42,14 @@ def as_str(arr):
 def as_bool(arr):
     return [ (x=='#') for x in arr]
 
-def count(text, c):
-    return sum([1 for i in range(len(text)) if text[i]==c ])
+def count(arr):
+    return sum([1 for x in arr if x==True ])
 
 def mprint(n,arr):
-    line = "".join([("#" if x else ".") for x in arr[:60]])
-    print("{:5d}: ".format(n)+line)
+    print("{:5d}: ".format(n)+as_str(arr))
 
-# initial_state = "#..#.#..##......###...###"
+
+initial_state = "#..#.#..##......###...###"
 initial_state = "##.#....#..#......#..######..#.####.....#......##.##.##...#..#....#.#.##..##.##.#.#..#.#....#.#..#.#"
 
 def any(arr):
@@ -68,6 +68,17 @@ state = []
 
 def apply():
     global origin
+
+    # shorten the front of the state as possible
+    if not any(state[:10]) :
+        del state[0]
+        del state[0]
+        del state[0]
+        del state[0]
+        del state[0]
+        origin -= 5
+        # mprint(222,state)
+
     # mprint(111,state)
     # grow the front of the state as necessary
     if any(state[:5]) :
@@ -104,20 +115,48 @@ def apply():
 
     return
 
+def value():
+    global origin
+    total = 0
+    for i in range(len(state)):
+        if state[i]: total += i-origin
+    return total
+
+def same(state1, state2):
+    if len(state1) != len(state2) : return False
+    for i in range(len(state1)):
+        if state1[i] != state2[i] : return False
+    return True
+
+import copy
+
 # convert initial state to a boolean state array
 state = [ (x=='#') for x in initial_state]
+print("[{:5d}] {:5d}: ".format(count(state),0)+as_str(state[:60]))
 mprint(0,state)
+prev_state = copy.deepcopy(state)
 generations = 50000000000
-for i in range(20):
-    apply()
-    if i+1 % 100 == 0 :
-        mprint(i+1,state)
+generations = 500000000
+generations = 30
+for i in range(100000):
+    apply() # changes the state
+    if (i<30) or ((i+1) % 10000) == 0 :
+        print("{:5d}: count={} len={} origin={} value={} :".format(i+1, count(state), len(state), origin, value())+as_str(state[-20:]))
+    # print("prev: "+str(prev_state))
+    # print("curr: "+str(state))
+    if same(prev_state,state) : 
+        print("steady state")
+        break
+    prev_state = copy.deepcopy(state)
+
 
 mprint(0,state)
         
 total = 0
 print("origin = ", origin)
-for i in range(len(state)):
-    if state[i]:
-        total += i-origin
-print(total)
+print("value = "+str(value()));
+
+# So, what is different in this solution is that we do NOT actually
+# let the program run to completion... that would take a VERY long
+# time.  BUT - we noticed a pattern that repeats and allows us to 
+# calculate the final value (using a spreadsheet)
